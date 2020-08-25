b1228ac88740c083f47feb6bf18e82297f61059b
piyush-sh19 <piyush.sh19@gmail.com>
2020-08-25 16:06:59 +0530

    initial commit

---
+++ src/SidebarChat.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SidebarChat.css";
+
+function SidebarChat() {
+  return <div className="sidebarchat"></div>;
+}
+
+export default SidebarChat;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUHNICswBfrs76C67xvMvx2Fd4y83iELM",
+  authDomain: "whatsapp-clone-b7d27.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-b7d27.firebaseio.com",
+  projectId: "whatsapp-clone-b7d27",
+  storageBucket: "whatsapp-clone-b7d27.appspot.com",
+  messagingSenderId: "66709033510",
+  appId: "1:66709033510:web:c2721ae171c0a138ad0ce7",
+  measurementId: "G-9RRJGRLNCD",
+};
