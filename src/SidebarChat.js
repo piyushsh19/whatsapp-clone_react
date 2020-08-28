7b73aecccdf1cfdb78c854f285fc81db72538ab4
piyush-sh19 <piyush.sh19@gmail.com>
2020-08-28 14:14:45 +0530

    commit

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./SidebarChat.css";
 import { Avatar } from "@material-ui/core";
+import db from "./firebase";
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState("");
 
   useEffect(() => {
@@ -12,14 +13,14 @@ function SidebarChat({ addNewChat }) {
   const createChat = () => {
     const roomName = prompt("Please enter name for chat");
     if (roomName) {
-      // db stug
+      db.collection("rooms").add({ name: roomName });
     }
   };
   return !addNewChat ? (
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
-        <h2>Room mane</h2>
+        <h2>{name}</h2>
         <p>Last message</p>
       </div>
     </div>
